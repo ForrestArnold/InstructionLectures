60a05b52d4f24defe1c7cce031374d7d4615121b
ForrestArnold <farnold3380@xmail.dixietech.edu>
2023-04-24 13:09:40 -0600

    Initial Commit

---
+++ InstructionLectures/InstructionLecturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstructionLecturesApp.swift
+//  InstructionLectures
+//
+//  Created by Student on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct InstructionLecturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
