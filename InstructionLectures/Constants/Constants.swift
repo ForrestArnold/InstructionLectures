cdb1d4479386c587f1e0b1e7c3403da8b09f78f2
ForrestArnold <farnold3380@xmail.dixietech.edu>
2023-04-24 15:52:20 -0600

    Finished Check Split
    For personal Check splitting

---
+++ InstructionLectures/Constants/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  InstructionLectures
+//
+//  Created by Student on 4/24/23.
+//
+
+import Foundation
+import SwiftUI
+
+// This file is to get rid of magic numbers in our app
+
+struct Constants {
+    static let navigationLinkFontSize: CGFloat = 24
+    static let buttonCornerRadius: CGFloat = 20.0
+    static let projectTitle: String = "Instructor Lectures"
+    static let randomHueRange: ClosedRange = 0.01...1.0
+    static let randomSaturationRange: ClosedRange = 0.3...1.0
+    static let randomBrightnessRange: ClosedRange = 0.5...1.0
+}
---
+++ InstructionLectures/ProjectSetup/HomeView.swift
@@ -0,0 +1,53 @@
+//
+//  HomeView.swift
+//  InstructionLectures
+//
+//  Created by Student on 4/24/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationStack {
+            List { // only 10 views can go into a list
+                Group {
+                    navigate(to: CheckSplit(), title: "CheckSplit", numberOfLecture: 1)
+                }
+            }
+            .navigationTitle(Constants.projectTitle)
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
+
+extension HomeView {
+    func navigate(to destination: some View, title: String, numberOfLecture: Int) -> some View {
+        
+        let randomHue: Double = Double.random(in: Constants.randomHueRange)
+        let randomSaturation: Double = Double.random(in: Constants.randomSaturationRange)
+        let randomBrightness: Double = Double.random(in: Constants.randomBrightnessRange)
+        
+        return HStack {
+            Text("\(numberOfLecture)")
+                .font(.system(size:
+                                Constants.navigationLinkFontSize))
+            NavigationLink {
+                destination
+            } label: {
+                Text(title)
+                    .font(.system(size:
+                                Constants.navigationLinkFontSize))
+            }
+        }
+        .padding()
+        .background(Color(hue: randomHue, saturation: randomSaturation, brightness: randomBrightness))
+        .cornerRadius(Constants.buttonCornerRadius)
+    }
+}
